df34cd434c4e4a6a74ead436161fdec599f508a4
Daniel Padilla <dproldan@daniel-Ubuntux64.(none)>
2013-01-13 12:54:49 +0100

    14jan2013

--- G2-vendor-blobs.mk
+++ G2-vendor-blobs.mk
@@ -12,39 +12,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+DEVICE_PATH = vendor/motorola/G2
+DEVICE_PREBUILT := ${DEVICE_PATH}/proprietary
+
+
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \
-    vendor/motorola/G2/proprietary/lib/libbt-aptx.so:/obj/lib/libbt-aptx.so \
-    vendor/motorola/G2/proprietary/lib/libbt-mp3.so:/obj/lib/libbt-mp3.so \
-    vendor/motorola/G2/proprietary/lib/libbt-mpeg12pl.so:/obj/lib/libbt-mpeg12pl.so \
+    ${DEVICE_PREBUILT}/lib/libbt-aptx.so:/obj/lib/libbt-aptx.so \
+    ${DEVICE_PREBUILT}/lib/libbt-mp3.so:/obj/lib/libbt-mp3.so \
+    ${DEVICE_PREBUILT}/lib/libbt-mpeg12pl.so:/obj/lib/libbt-mpeg12pl.so \
 
 
 
-# All the blobs necessary 
+# All the blobs that appear in the stock init.rc and init.mapphone_umts.rc 
 
 PRODUCT_COPY_FILES += \
-    vendor/motorola/G2/proprietary/etc/install-recovery.sh:/system/etc/install-recovery.sh \
-    vendor/motorola/G2/proprietary/lib/libbt-aptx.so:/system/lib/libbt-aptx.so \
-    vendor/motorola/G2/proprietary/lib/libbt-mp3.so:/system/lib/libbt-mp3.so \
-    vendor/motorola/G2/proprietary/lib/libbt-mpeg12pl.so:/system/lib/libbt-mpeg12pl.so \
-    vendor/motorola/G2/proprietary/bin/wlan_loader:/system/bin/wlan_loader \
-    vendor/motorola/G2/proprietary/etc/wifi/fw_tiwlan_ap.bin:/system/etc/wifi/fw_tiwlan_ap.bin \
-    vendor/motorola/G2/proprietary/etc/wifi/tiwlan_ap.ini:/system/etc/wifi/tiwlan_ap.ini \
-    vendor/motorola/G2/proprietary/etc/wifi/fw_tiwlan_ap_rfmd.bin:/system/etc/wifi/fw_tiwlan_ap_rfmd.bin \
-    vendor/motorola/G2/proprietary/etc/wifi/tiwlan_ap_rfmd.ini:/system/etc/wifi/tiwlan_ap_rfmd.ini \
-    vendor/motorola/G2/proprietary/bin/Hostapd:/system/bin/Hostapd \
-    vendor/motorola/G2/proprietary/bin/mount_ext3.sh:/system/bin/mount_ext3.sh \
-    vendor/motorola/G2/proprietary/lib/modules/sec.ko:/system/lib/modules/sec.ko \
-    vendor/motorola/G2/proprietary/bin/load_modules.sh:/system/bin/load_modules.sh \
-    vendor/motorola/G2/proprietary/lib/modules/snd-soc-cs48l10.ko:/system/lib/modules/snd-soc-cs48l10.ko \
-    vendor/motorola/G2/proprietary/bin/startup_smc.sh:/system/bin/startup_smc.sh \
-    vendor/motorola/G2/proprietary/lib/modules/vpnclient.ko:/system/lib/modules/vpnclient.ko \
-    vendor/motorola/G2/proprietary/bin/mot_boot_mode:/system/bin/mot_boot_mode \
-    vendor/motorola/G2/proprietary/bin/dbvc_atvc_property_set:/system/bin/dbvc_atvc_property_set \
-    vendor/motorola/G2/proprietary/bin/usbd:/system/bin/usbd \
-    vendor/motorola/G2/proprietary/bin/msp430:/system/bin/msp430 \
-    vendor/motorola/G2/proprietary/bin/gkisystem:/system/bin/gkisystem \
-    vendor/motorola/G2/proprietary/bin/rild_tcmd:/system/bin/rild_tcmd \
+    ${DEVICE_PREBUILT}/etc/install-recovery.sh:/system/etc/install-recovery.sh \
+    ${DEVICE_PREBUILT}/lib/libbt-aptx.so:/system/lib/libbt-aptx.so \
+    ${DEVICE_PREBUILT}/lib/libbt-mp3.so:/system/lib/libbt-mp3.so \
+    ${DEVICE_PREBUILT}/lib/libbt-mpeg12pl.so:/system/lib/libbt-mpeg12pl.so \
+    ${DEVICE_PREBUILT}/bin/wlan_loader:/system/bin/wlan_loader \
+    ${DEVICE_PREBUILT}/etc/wifi/fw_tiwlan_ap.bin:/system/etc/wifi/fw_tiwlan_ap.bin \
+    ${DEVICE_PREBUILT}/etc/wifi/tiwlan_ap.ini:/system/etc/wifi/tiwlan_ap.ini \
+    ${DEVICE_PREBUILT}/etc/wifi/fw_tiwlan_ap_rfmd.bin:/system/etc/wifi/fw_tiwlan_ap_rfmd.bin \
+    ${DEVICE_PREBUILT}/etc/wifi/tiwlan_ap_rfmd.ini:/system/etc/wifi/tiwlan_ap_rfmd.ini \
+    ${DEVICE_PREBUILT}/bin/Hostapd:/system/bin/Hostapd \
+    ${DEVICE_PREBUILT}/bin/mount_ext3.sh:/system/bin/mount_ext3.sh \
+    ${DEVICE_PREBUILT}/lib/modules/sec.ko:/system/lib/modules/sec.ko \
+    ${DEVICE_PREBUILT}/bin/load_modules.sh:/system/bin/load_modules.sh \
+    ${DEVICE_PREBUILT}/lib/modules/snd-soc-cs48l10.ko:/system/lib/modules/snd-soc-cs48l10.ko \
+    ${DEVICE_PREBUILT}/bin/startup_smc.sh:/system/bin/startup_smc.sh \
+    ${DEVICE_PREBUILT}/lib/modules/vpnclient.ko:/system/lib/modules/vpnclient.ko \
+    ${DEVICE_PREBUILT}/bin/mot_boot_mode:/system/bin/mot_boot_mode \
+    ${DEVICE_PREBUILT}/bin/dbvc_atvc_property_set:/system/bin/dbvc_atvc_property_set \
+    ${DEVICE_PREBUILT}/bin/usbd:/system/bin/usbd \
+    ${DEVICE_PREBUILT}/bin/msp430:/system/bin/msp430 \
+    ${DEVICE_PREBUILT}/bin/gkisystem:/system/bin/gkisystem \
+    ${DEVICE_PREBUILT}/bin/rild_tcmd:/system/bin/rild_tcmd \
     
+# copy all of our kernel modules under the "modules" directory to system/lib/modules
+PRODUCT_COPY_FILES += $(shell test -d vendor/motorola/G2/proprietary/lib/modules && \
+	find vendor/motorola/G2/proprietary/lib/modules -name '*.ko' \
+	-printf '%p:system/lib/modules/%f ')
 
+#key layouts, names must fit the ones in /proc/bus/input/devices, qwerty.kl is the fallback one.
+PRODUCT_COPY_FILES += \
+	${DEVICE_PREBUILT}/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+	${DEVICE_PREBUILT}/usr/keylayout/hs-key.kl:system/usr/keylayout/hs-key.kl \
+	${DEVICE_PREBUILT}/usr/keylayout/sholes-keypad.kl:system/usr/keylayout/sholes-keypad.kl \
+	${DEVICE_PREBUILT}/usr/keylayout/G2-keypad.kl:system/usr/keylayout/G2-keypad.kl \
+	${DEVICE_PREBUILT}/usr/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+	${DEVICE_PREBUILT}/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
 
+#copy hw libs under system/lib/hw
+PRODUCT_COPY_FILES += \
+    ${DEVICE_PREBUILT}/lib/hw/alsa.G2.so:system/lib/hw/alsa.G2.so \
+    ${DEVICE_PREBUILT}/lib/hw/gps.G2.so:system/lib/hw/gps.G2.so \
+    ${DEVICE_PREBUILT}/lib/hw/kinetics.G2.so:system/lib/hw/kinetics.G2.so \
+    ${DEVICE_PREBUILT}/lib/hw/lights.G2.so:system/lib/hw/lights.G2.so \
+    ${DEVICE_PREBUILT}/lib/hw/sensors.G2.so:system/lib/hw/sensors.G2.so \
+    ${DEVICE_PREBUILT}/lib/hw/gralloc.omap3.so:system/lib/hw/gralloc.omap3.so \
+
+#copy hw libs under system/etc/firmware
+PRODUCT_COPY_FILES += \
+    ${DEVICE_PREBUILT}/etc/firmware/cs48l10.bin:system/etc/firmware/cs48l10.bin \
+    ${DEVICE_PREBUILT}/etc/firmware/wl1271.bin:system/etc/firmware/wl1271.bin \
+    ${DEVICE_PREBUILT}/etc/firmware/wl1271_ANT.bin:system/etc/firmware/wl1271_ANT.bin \
+    ${DEVICE_PREBUILT}/etc/firmware/wl1271_LE.bin:system/etc/firmware/wl1271_LE.bin \
+
+#copy hw libs under system/etc/mspfirmware
+PRODUCT_COPY_FILES += \
+    ${DEVICE_PREBUILT}/etc/mspfirmware/mspfirmware.bin:system/etc/mspfirmware/mspfirmware.bin \
+    ${DEVICE_PREBUILT}/etc/mspfirmware/mspfirmwarefactory.bin:system/etc/mspfirmware/mspfirmwarefactory.bin \
+    ${DEVICE_PREBUILT}/etc/mspfirmware/version.txt:system/etc/mspfirmware/version.txt \
