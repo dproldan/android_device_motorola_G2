6f21b027737eec27d36977bf05324c669da37df4
Daniel Padilla <dproldan@daniel-Ubuntux64.(none)>
2013-01-13 03:19:39 +0100

    13jan2013

--- G2-vendor-blobs.mk
+++ G2-vendor-blobs.mk
@@ -27,5 +27,24 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/G2/proprietary/lib/libbt-aptx.so:/system/lib/libbt-aptx.so \
     vendor/motorola/G2/proprietary/lib/libbt-mp3.so:/system/lib/libbt-mp3.so \
     vendor/motorola/G2/proprietary/lib/libbt-mpeg12pl.so:/system/lib/libbt-mpeg12pl.so \
+    vendor/motorola/G2/proprietary/bin/wlan_loader:/system/bin/wlan_loader \
+    vendor/motorola/G2/proprietary/etc/wifi/fw_tiwlan_ap.bin:/system/etc/wifi/fw_tiwlan_ap.bin \
+    vendor/motorola/G2/proprietary/etc/wifi/tiwlan_ap.ini:/system/etc/wifi/tiwlan_ap.ini \
+    vendor/motorola/G2/proprietary/etc/wifi/fw_tiwlan_ap_rfmd.bin:/system/etc/wifi/fw_tiwlan_ap_rfmd.bin \
+    vendor/motorola/G2/proprietary/etc/wifi/tiwlan_ap_rfmd.ini:/system/etc/wifi/tiwlan_ap_rfmd.ini \
+    vendor/motorola/G2/proprietary/bin/Hostapd:/system/bin/Hostapd \
+    vendor/motorola/G2/proprietary/bin/mount_ext3.sh:/system/bin/mount_ext3.sh \
+    vendor/motorola/G2/proprietary/lib/modules/sec.ko:/system/lib/modules/sec.ko \
+    vendor/motorola/G2/proprietary/bin/load_modules.sh:/system/bin/load_modules.sh \
+    vendor/motorola/G2/proprietary/lib/modules/snd-soc-cs48l10.ko:/system/lib/modules/snd-soc-cs48l10.ko \
+    vendor/motorola/G2/proprietary/bin/startup_smc.sh:/system/bin/startup_smc.sh \
+    vendor/motorola/G2/proprietary/lib/modules/vpnclient.ko:/system/lib/modules/vpnclient.ko \
+    vendor/motorola/G2/proprietary/bin/mot_boot_mode:/system/bin/mot_boot_mode \
+    vendor/motorola/G2/proprietary/bin/dbvc_atvc_property_set:/system/bin/dbvc_atvc_property_set \
+    vendor/motorola/G2/proprietary/bin/usbd:/system/bin/usbd \
+    vendor/motorola/G2/proprietary/bin/msp430:/system/bin/msp430 \
+    vendor/motorola/G2/proprietary/bin/gkisystem:/system/bin/gkisystem \
+    vendor/motorola/G2/proprietary/bin/rild_tcmd:/system/bin/rild_tcmd \
+    
 
 
